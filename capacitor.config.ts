b3120841c037c2f4bb805c047f5d603c0cc4956f
Z4F1 <pelle.pastoor217@gmail.com>
2021-10-01 21:59:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Stacc Challenge',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
